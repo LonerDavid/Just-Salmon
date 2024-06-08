5cefc9a5b831d140ee5e7ce3c0ddf85b8071858c
Loner David <davidyang19995@gmail.com>
2024-06-08 11:35:50 +0800

    Project File Update

---
+++ Just-Salmon_App/Just-Salmon_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Just-Salmon_App
+//
+//  Created by Loner David on 2024/6/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Just-Salmon_App/Just-Salmon_App/Just_Salmon_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_Salmon_AppApp.swift
+//  Just-Salmon_App
+//
+//  Created by Loner David on 2024/6/8.
+//
+
+import SwiftUI
+
+@main
+struct Just_Salmon_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
