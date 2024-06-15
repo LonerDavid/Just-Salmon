bcf234befd7992310d9372ed9aa2e6a4113c1a16
Loner David <davidyang19995@gmail.com>
2024-06-15 16:44:51 +0800

    Add missing file dependency

--- Just-Salmon_App/Just-Salmon_App/JoinPage.swift
+++ Just-Salmon_App/Just-Salmon_App/JoinPage.swift
@@ -9,36 +9,46 @@ import SwiftUI
 
 struct JoinPage: View {
   var body: some View {
-    VStack{
-      Spacer()
-      Text("Track,\nAdjust,\nSucceed.")
-        .lineSpacing(10.0)
-        .multilineTextAlignment(.leading)
-        .font(.system(size: 74, weight: .heavy))
-//        .bold()
-      HStack{
+    NavigationStack{
+      VStack{
+        
         Spacer()
-        Button {
+        Text("Track,\nAdjust,\nSucceed.")
+          .lineSpacing(10.0)
+          .multilineTextAlignment(.leading)
+          .font(.system(size: 74, weight: .heavy))
+        HStack{
+          Spacer()
           
-        } label: {
-          HStack(spacing: 4) {
-            Text("Join")
-            Image(systemName: "arrow.right")
+          NavigationLink(value: Destination.mainPage){
+            HStack(spacing: 4) {
+              Text("Join")
+              Image(systemName: "arrow.right")
+            }
+            .font(.title)
+            .foregroundStyle(.white)
+            .padding(.vertical, 16)
+            .padding(.horizontal, 32)
+            .background(.orange)
+            .clipShape(Capsule())
           }
-          .font(.title)
-          .foregroundStyle(.white)
-          .padding(.vertical, 16)
-          .padding(.horizontal, 32)
-          .background(.orange)
-          .clipShape(Capsule())
+        }
+      }
+      .padding(.horizontal, 32)
+      .padding(.vertical, 8)
+      .navigationDestination(for: Destination.self) { destination in
+        switch destination {
+        case .mainPage:
+          Text("Main Page")
+            .toolbar(.hidden, for: .navigationBar)
         }
       }
     }
-    .padding(.horizontal, 32)
-    .padding(.vertical, 8)
-//    .background(.blue)
-    
   }
+}
+
+enum Destination{
+  case mainPage
 }
 
 #Preview {
